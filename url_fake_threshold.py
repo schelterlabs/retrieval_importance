e7b2a40ea934a3178098d877fab450dd9fe24307
Xiaozhong Lyu <xialyu@eu-login-10.euler.ethz.ch>
2023-05-04 14:58:11 +0200

    try new lr

--- url_fake_threshold.py
+++ url_fake_threshold.py
@@ -65,7 +65,7 @@ def experiment(random_seed, retrievals, K = 10, threshold = 0.5):
     #compute the excution time of the algorithm
     encoded_retrievals, mapping = encode_retrievals(val_retrievals, "retrieved_websites", "retrieved_answers", utility)
     grouping, group_mapping = encode_groups(mapping, group)
-    v_ungrouped = learn_importance(encoded_retrievals, k=K, learning_rate=0.5, num_steps=10, n_jobs=-1, grouping=grouping)
+    v_ungrouped = learn_importance(encoded_retrievals, k=K, learning_rate=500, num_steps=50, n_jobs=-1, grouping=grouping)
     v = v_grouped(v_ungrouped, grouping, group_mapping)
 
     url_count = {}
@@ -116,8 +116,8 @@ def work_load(relation_name):
             retrievals.append(json.loads(line))
 
     result_list = []
-    for random_seed in [441, 1, 469, 53, 280, 123, 219, 181, 5, 9, 199, 156, 93, 313, 28, 56, 359, 108, 8, 58, 407, 451, 322, 266, 268, 297, 12, 182, 320, 474, 296, 142, 64, 201, 32, 392, 98, 242, 344, 438, 427, 35, 77, 394, 39, 55, 330, 38, 67, 358, 237, 149, 405, 420, 411, 57, 488, 49, 42, 155, 109, 73, 331, 128]:
-    # for random_seed in [441, 1, 469, 53, 280, 123, 219, 181, 5, 9, 199, 156, 93, 313, 28, 56]:
+    # for random_seed in [441, 1, 469, 53, 280, 123, 219, 181, 5, 9, 199, 156, 93, 313, 28, 56, 359, 108, 8, 58, 407, 451, 322, 266, 268, 297, 12, 182, 320, 474, 296, 142, 64, 201, 32, 392, 98, 242, 344, 438, 427, 35, 77, 394, 39, 55, 330, 38, 67, 358, 237, 149, 405, 420, 411, 57, 488, 49, 42, 155, 109, 73, 331, 128]:
+    for random_seed in [441, 1, 469, 53, 280, 123, 219, 181, 5, 9, 199, 156, 93, 313, 28, 56]:
         result_list.append(experiment(random_seed, retrievals, K = 10, threshold = 0.5))
     
     acc_baseline = sum([i[0] for i in result_list])/len(result_list)
@@ -132,7 +132,7 @@ if __name__ == '__main__':
     r = ['applies_to_jurisdiction', 'author', 'award_received', 'basic_form_of_government', 'capital', 'capital_of', 'composer', 'continent', 'country', 'country_of_citizenship', 'country_of_origin', 'creator', 'currency', 'developer', 'director', 'discoverer_or_inventor', 'drug_or_therapy_used_for_treatment', 'educated_at', 'employer', 'field_of_work', 'genetic_association', 'genre', 'has_part', 'head_of_government', 'head_of_state', 'headquarters_location', 'industry', 'influenced_by', 'instance_of', 'instrument', 'language_of_work_or_name', 'languages_spoken_written_or_signed', 'located_in_the_administrative_territorial_entity', 'location', 'location_of_discovery', 'location_of_formation', 'majority_opinion_by', 'manufacturer', 'measured_physical_quantity', 'medical_condition_treated', 'member_of', 'member_of_political_party', 'member_of_sports_team', 'movement', 'named_after', 'native_language', 'occupation', 'office_held_by_head_of_government', 'official_language', 'operating_system', 'original_language_of_film_or_TV_show', 'original_network', 'owned_by', 'part_of', 'participating_team', 'place_of_birth', 'place_of_death', 'position_held', 'position_played_on_team', 'programming_language', 'recommended_unit_of_measurement', 'record_label', 'religion', 'shares_border_with', 'stock_exchange', 'subclass_of', 'subsidiary', 'symptoms_and_signs', 'twinned_administrative_body', 'work_location']
     # r = ["author"]
     result_list = []    
-    with open("./test_data/result/wiki_fake_threshold_64.jsonl", "a") as f:
+    with open("./test_data/result/wiki_fake_threshold_8_500_50.jsonl", "w") as f:
         for i in r:
             acc_baseline, acc_retrieval, acc_threshold = work_load(i)
 
