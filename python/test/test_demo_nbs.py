1d5de0abb0990db8bfd956113d7d8b9b6ac26079
sscdotopen <ssc.open@googlemail.com>
2023-05-21 13:45:17 +0200

    more cleanup

--- python/test/test_demo_nbs.py
+++ python/test/test_demo_nbs.py
@@ -9,13 +9,12 @@ TOY_EXAMPLE_NB = os.path.join(str(get_project_root()), "toy_example.ipynb")
 TOY_EXAMPLE_GROUPS_NB = os.path.join(str(get_project_root()), "toy_example_with_groups.ipynb")
 WIKIFACT_NB = os.path.join(str(get_project_root()), "wikifact.ipynb")
 WIKIFACT_GROUPS_NB = os.path.join(str(get_project_root()), "wikifact_with_groups.ipynb")
-TIFU_TOY_EXAMPLE_NB = os.path.join(str(get_project_root()), "tifu_toy_example.ipynb")
+
 
 def test_demo_nbs():
     """
     Tests whether the demo notebook works
     """
     # matplotlib.use("template")  # Can be used if we have matplotlib plots in the demo notebooks at some point
-    for notebook in [TOY_EXAMPLE_NB, TOY_EXAMPLE_GROUPS_NB, WIKIFACT_GROUPS_NB, WIKIFACT_GROUPS_NB,
-                     TIFU_TOY_EXAMPLE_NB]:
+    for notebook in [TOY_EXAMPLE_NB, TOY_EXAMPLE_GROUPS_NB, WIKIFACT_GROUPS_NB, WIKIFACT_GROUPS_NB]:
         Notebook().load(notebook)
